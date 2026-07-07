96fa2838d6918d05d42d09878d0216741985778b
Josef Vyhnálek <vyhnalek.josef1@gmail.com>
2026-07-07 14:17:59 +0200

    simplify env name

--- scripts/env-decryption.sh
+++ scripts/env-decryption.sh
@@ -7,8 +7,8 @@ APP_NAME="$1"
 ENVIRONMENT="$2"
 
 
-sops -d "envs/$APP_NAME.enc.env" > "apps/$APP_NAME/$APP_NAME.env" #hodit to potom do složky repo-1 abych se zbavil ../.. - dát to vedle compose filu
+sops -d "envs/$APP_NAME.enc.env" > "apps/$APP_NAME/.env" #hodit to potom do složky repo-1 abych se zbavil ../.. - dát to vedle compose filu
 
 if [ -n "$ENVIRONMENT" ]; then
-  sops -d "envs/$ENVIRONMENT/$APP_NAME.enc.env" > "apps/$APP_NAME/$APP_NAME.$ENVIRONMENT.env" # tady ještě bude muset být název jinej
+  sops -d "envs/$ENVIRONMENT/$APP_NAME.enc.env" > "apps/$APP_NAME/.$ENVIRONMENT.env" # tady ještě bude muset být název jinej
 fi
